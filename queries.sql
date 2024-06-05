efc15e6280722011471fa5ea372bbe143f98679c
Nicole Wade <nwade0916@gmail.com.com>
2024-06-05 00:24:34 -0500

    Task 2 complete

--- queries.sql
+++ queries.sql
@@ -1,6 +1,17 @@
---Part 1
+-- Part 1: Job Table Columns and Data Types
+-- id: INTEGER
+-- name: VARCHAR(255)
+-- employer_id: INTEGER
+-- location: VARCHAR(255)
+-- core_competency: VARCHAR(255)
+-- position_type: VARCHAR(255)
+-- skill_id: INTEGER
+-- skill_id: INTEGER
+-- position_type_id: INTEGER
 
---Part 2
+
+-- Part 2: List the names of the employers in St. Louis City
+SELECT name FROM employer WHERE location = "St. Louis City";
 
 --Part 3
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 500, message = "Description must be less than or equal to 500 characters")
+    private String description;
+
+    // No-arg constructor required by Hibernate
+    public Skill() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+
+    }
 }
